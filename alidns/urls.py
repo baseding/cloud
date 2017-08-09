df192b00fefd4b7c44f15fee2a3180eceefcf911
baseding <baseding@qq.com>
2017-08-09 15:26:53 +0800

    20170809-alidns-v0.1.ok

--- alidns/urls.py
+++ alidns/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # ex: /alidns/
     url(r'^$', views.index, name='index'),
     url(r'^records/$', views.records, name='records'),
+    url(r'^add/$', views.add, name='add'),
 ]
 
