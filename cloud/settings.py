9565ec7fa3e6abc769cd72c343b8cf7887c80391
baseding <baseding@qq.com>
2017-09-20 13:51:53 +0800

    20170920-httpd-sshhostip.ok

--- cloud/settings.py
+++ cloud/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'USER': 'yfding',
         'PASSWORD': 'yfding',
         'HOST': '127.0.0.1',
-        'PORT': '3307',
+        'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRIcT_TRANS_TABLES'",
             'charset': 'utf8mb4',
